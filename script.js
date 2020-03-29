50fa18883cb34a56633ba952238ad17b5c4808ef
kahae-bit <58144108+kahae-bit@users.noreply.github.com>
2020-03-29 14:44:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+// question 1
+
+const cat = {
+	complain: "Meow!"
+};
+
+// question 2
+
+const subheading1 = document.querySelector("h3");
+console.dir(subheading);
+
+// question 3
+
+const subheading1 = document.querySelector("h3")
+subheading.style.fontSize = "2em";
+
+// question 4
+
+const subheading1 = document.querySelector("h3");
+div.classList.add("subheading");
+console.log(div.classSubheading);
+
+// question 5
+
+const paragraph = document.querySelector("p");
+
+// question 6
+
+const resultsContainer = document.querySelector("div");
+resultsContainer.innterHTML = <p>New paragraph</p>;
+
+// question 7
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function logWord(catArray) {
+	console.log(catArray);
+}
+
+logWord("Blob");
+logWord("Harold");
+logWord("Blurt");
+
+// question 8
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function logWord(catArray) {
+	console.log(catArray);
+}
+
+logWord("Blob");
+logWord("Harold");
+logWord("Blurt");
+
+for (let i = 0; i < .lenght; i++) {
+	const name = "<h5>" + catArray[i].name + "</h5>";
+console.log(name);
